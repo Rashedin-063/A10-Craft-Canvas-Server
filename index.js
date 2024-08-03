242ecad504d6deb602c1ec1962ec21dee40b18d1
RU063 <rashedinislam.06@gmail.com>
2024-08-03 15:02:26 +0600

    users get and patch method implemented

--- index.js
+++ index.js
@@ -31,9 +31,39 @@ async function run() {
     const userCollection = client.db('craftCanvasDB').collection('users')
 
     // user related api
+
+    app.get('/users', async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.get('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.findOne(query);
+
+      res.send(result)
+    })
+
     app.post('/users', async (req, res) => {
       const user = req.body;
       const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
+
+    app.patch('/users', async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user.email }
+      const updatedUser = {
+        $set: {
+lastLoggedIn: user.lastSignIn
+        },
+      }
+
+      const result = await userCollection.updateOne(query, updatedUser);
+
       res.send(result)
     })
 
